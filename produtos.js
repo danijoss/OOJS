5d4833c08c8459e5c7f9813fac1cc4565ef1743d
Daniel <dan.henrri@gmail.com>
2023-07-22 13:09:07 -0300

    commit submissao

--- produtos.js
+++ produtos.js
@@ -1,5 +1,6 @@
-function Produto(codigo, preco){
+function Produto(codigo, modelo, preco){
     this.codigo = codigo;
+    this.modelo = modelo;
     let _preco = preco;
 
     this.getPreco = function() {
@@ -12,25 +13,29 @@ function Produto(codigo, preco){
     }
 }
 
-function Tenis(codigo, modelo, preco){
-    this.modelo = modelo;
+function Tenis(codigo, modelo, preco, numeracao){
+    this.numeracao = numeracao;
     
-    Produto.call(this, codigo, preco);
+    Produto.call(this, codigo, modelo, preco);
 }
 
-function Roupa(codigo, marca, preco){
-    this.marca = marca;
+function Roupa(codigo, modelo, preco, tamanho){
+    this.tamanho = tamanho;
     
-    Produto.call(this, codigo, preco);
+    Produto.call(this, codigo, modelo, preco);
 }
 
-const tenis1 = new Tenis(123, "Air Jordan 1", 500);
-const tenis2 = new Tenis(456, "Suede", 400);
+const tenis1 = new Tenis(123, "Air Jordan 1", 500, "34-45");
+const tenis2 = new Tenis(456, "Suede", 400, "33-44");
 
-const camisa1 = new Roupa(789, "Camisa Adidas", 150)
+const camisa1 = new Roupa(789, "Camisa Adidas", 150, "pp-p-m-g-gg")
 
 console.log(tenis1)
 console.log('valor do tenis: '+ tenis1.getPreco())
+
+tenis1.setPreco(600)
+
+console.log('novo valor do tenis: '+ tenis1.getPreco())
 
 console.log(tenis2)
 console.log(camisa1)
